ed6eff2a3490f542fa665c6fde8d9ea52c3b1a8a
ReinhardDr <emlamanh2002@gmail.com>
2022-10-17 12:08:44 +0700

    add icons into bottom bar

--- app/screens/Navigation/NavigatorApp.js
+++ app/screens/Navigation/NavigatorApp.js
@@ -2,15 +2,29 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AudioList from '../audiolist';
 import player from '../player';
-import playelist from '../playerlist';
 import playerlist from '../playerlist';
+import { Entypo } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+
 
 const Tab = createBottomTabNavigator()
 const NavigatorApp =() => {
     return <Tab.Navigator>
-        <Tab.Screen name = 'AudioList' component={AudioList} />
-        <Tab.Screen name = 'player' component={player} />
-        <Tab.Screen name = 'playerlist' component={playerlist}/>
+        <Tab.Screen name = 'HomePage' component={AudioList} options ={{
+            tabBarIcon: ({color , size,focused}) => {
+                return <Entypo name="home" size={24} color="black" />
+            }
+        }} />
+        <Tab.Screen name = 'Player' component={player} options ={{
+            tabBarIcon: ({size ,color}) => {
+                return <Entypo name="music" size={24} color="black" />
+            }
+        }} />
+        <Tab.Screen name = 'List Music' component={playerlist} options ={{
+            tabBarIcon: ({size, color}) =>{
+                return <Ionicons name="library-sharp" size={24} color="black" />
+            }
+        }} />
     </Tab.Navigator>
 }
 export default NavigatorApp;
